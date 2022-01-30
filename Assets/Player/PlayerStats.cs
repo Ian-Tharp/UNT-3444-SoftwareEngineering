4c6d44f4cb41868269b6e3eddf275eddc247fb9a
Ian Tharp <praht09ian@gmail.com>
2022-01-30 14:14:58 -0600

    Added Basic Player Functions & Tower Sprites
    Sprites added for defense turrets, worked on the outline for a prefab for turrets to allow for spawning of missiles from the exact location of the sprite, as well as having separate sprites so turrets can rotate directly towards enemies.

--- Assets/Player/PlayerStats.cs
+++ Assets/Player/PlayerStats.cs
@@ -1,18 +1,54 @@
+using System.Net.Mime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public int Currency = 0;
+    public int TotalHealth = 100;
+    public int CurrentHealth = TotalHealth;
+    public int CurrentShields = 0;
+    public int TotalShields = 0;
+
+    bool hasShield = false;
+    bool isDead = false;
+
+
+    void SubtractHealth(int Amount) {
+        CurrentHealth -= Amount;
+        if (CurrentHealth <= 0) {
+            isDead = true;
+        }
+    }
+    
+    void AddCurrency(int Amount) {
+        Currency += Amount;
+        //Max Currency Check
+        if (Currency > 999999) {
+            Currency = 999999;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    bool SubtractCurrency(int Amount) {
+        if (Currency - Amount >= 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    void ResetHealth() { CurrentHealth = TotalHealth; }
+    void EnableShields() { hasShield = true; }
+
+    //Called once every frame
+    void Update() {
+        //Checks if player isDead bool is true
+        //If so, quit the game for now
+        //We will implement death screen functionality later
+        if (isDead) {
+            Application.Quit();
+        }
     }
 }
