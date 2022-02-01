dfc5a70fd30d2694da1d20d401729c8a0ebfdd76
Ian Tharp <IanTharp2@my.unt.edu>
2022-02-01 12:23:33 -0600

    Wave Manager Updates
    Also removed testing.html
    Created framework for spawning waves ability to reset waves.

--- Assets/WaveSystem/WaveManager.cs
+++ Assets/WaveSystem/WaveManager.cs
@@ -5,19 +5,47 @@ using UnityEngine;
 public class StartWave : MonoBehaviour
 {
     public int WaveNumber;
+    public int EnemyCount;
+    public int TotalEnemyCount;
+    public int EnemyIndex;
+    public int RemainingEnemyCount;
+    public bool CanSpawnWave = false;
+    int[] EnemyValue = new int[] {1, 2, 3, 5, 10};
     int TotalWaveScore;
 
-    void CalculateWaveScore() {
 
+    //Used to reset wave manager or restart game
+    void ResetWaveManager() {
+        WaveNumber = 0;
+        EnemyCount = 0;
+        TotalEnemyCount = 0;
+        RemainingEnemyCount = 0;
+        CanSpawnWave = false;
+        TotalWaveScore = 0;
+    }
+
+    void DetermineWaveScore() {
+        TotalWaveScore = 10 + WaveNumber * 3;
+    }
+
+    void SubtractWaveScore(int Amount) {
+        TotalWaveScore -= Amount;
+        if (TotalWaveScore <= 0) {
+            TotalWaveScore = 0;
+            CanSpawnWave = true;
+        }
     }
 
     // Start is called before the first frame update
     void Start() {
         WaveNumber = 1;
+        DetermineWaveScore();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (TotalWaveScore > 0) {
+            CanSpawnWave = false;
+        }
     }
 }
