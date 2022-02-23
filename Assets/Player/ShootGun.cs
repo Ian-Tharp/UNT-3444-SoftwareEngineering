6946f0f2e99ea5496c3f07cfebb5ba2726094308
bethebullet <pokemonmasterbret@gmail.com>
2022-02-22 21:15:30 -0600

    Got Mouse Click to detect when it hits a target

--- Assets/Player/ShootGun.cs
+++ Assets/Player/ShootGun.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ShootGun : MonoBehaviour
 {
@@ -10,16 +11,25 @@ public class ShootGun : MonoBehaviour
     //    if so, deal damage and update enemy health
 
     // Update is called once per frame
+    Vector2 mousePosition;
+    
     void Update() {
-
+        mousePosition = Mouse.current.position.ReadValue();
     }
+
     public void Fire() // called anytime player fires
-    {
-        Debug.Log("Fire!");
+    {  
 
-        //vector2 mousePosition = playerInput.Player.Look.ReadValue<Vector2>;
+       Debug.Log("Fire!");
+       Debug.Log("Vector 2 location of Mouse: " + mousePosition);
 
-       //This is a test
+       // if mousePosition is by enemy position, then hit
+
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(mousePosition), Vector2.zero);
+        if (hit)
+        {
+            Debug.Log("Raycast Hit");
+        }          
     }
     
 
