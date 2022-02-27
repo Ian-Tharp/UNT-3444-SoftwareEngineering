3419d82a4d3941d9e4bf1cc807ba3bb64b77c0b3
ex-death <45133210+ex-death@users.noreply.github.com>
2022-02-27 14:10:16 -0600

    Added Menu Assets and LeanTween Assets

---
+++ Assets/Menu/StartMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    void Start()
+    {
+        transform.LeanMoveLocal(new Vector2 (0, 2.56062), 1).setEaseInOutBack;
+    }
+}
