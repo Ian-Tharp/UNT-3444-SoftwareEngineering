6b268a37d2f8d28b9d7826013038f9ce029a8b7d
bethebullet <pokemonmasterbret@gmail.com>
2022-04-19 22:22:03 -0500

    Turrets added to card system

--- Assets/Scripts/CardMenu.cs
+++ Assets/Scripts/CardMenu.cs
@@ -12,6 +12,12 @@ public class CardMenu : MonoBehaviour
     private GameObject stop;
     private GameObject inventory;
 
+    public GameObject tower1;
+    public GameObject tower2;
+    public GameObject tower3;
+    public GameObject tower4;
+    bool towerFull = false;
+
     public Text card1Txt;
     public Text card2Txt;
     
@@ -22,8 +28,8 @@ public class CardMenu : MonoBehaviour
 
     int card1ID;
     int card2ID;
-    int deckSize = 4;
-    string[] deck = new string[] {"Health", "Damage", "Regen", "Ammo Drum"};
+    int deckSize = 5;
+    string[] deck = new string[] {"Health", "Damage", "Regen", "Ammo Drum", "Turret"};
    
 
     void Start()
@@ -50,6 +56,10 @@ public class CardMenu : MonoBehaviour
 
     public void cardSelection()
     {
+        if (towerFull)
+        {
+            deckSize = deckSize - 1;
+        }
         card1ID = Random.Range(0, deckSize);
 
         card2ID = Random.Range(0, deckSize);
@@ -61,6 +71,10 @@ public class CardMenu : MonoBehaviour
         card1Txt.text = deck[card1ID];
 
        card2Txt.text = deck[card2ID];
+       if (towerFull)
+       {
+        deckSize = deckSize + 1;
+       }
     }
     
     public void cardChosen(Text name)
@@ -80,6 +94,10 @@ public class CardMenu : MonoBehaviour
         else if (name.text == "Ammo Drum")
         {
             ammoUp();
+        }
+        else if (name.text == "Turret")
+        {
+            defense();
         }
         cardMenu.SetActive(false);
     }
@@ -113,5 +131,27 @@ public class CardMenu : MonoBehaviour
             es.weaponInv[i].ammo = (int)(1.5 * es.weaponInv[i].ammo);
        }
        p.paused = false;
+   }
+
+   public void defense()
+   {
+       if (!tower1.activeSelf)
+       {
+           tower1.SetActive(true);
+       }
+       else if (!tower2.activeSelf)
+       {
+           tower2.SetActive(true);
+       }
+        else if (!tower3.activeSelf)
+       {
+           tower3.SetActive(true);
+       }
+        else if (!tower4.activeSelf)
+       {
+           tower4.SetActive(true);
+            towerFull = true;
+       }
+        p.paused = false;
    }
 }
